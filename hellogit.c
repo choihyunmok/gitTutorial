1b2efb94ce3b45db6fcdafc2570d85cdc285fa90
choihyunmok <37583279+choihyunmok@users.noreply.github.com>
2019-05-07 16:25:54 +0900

    Update hellogit.c
    function added: distinguish between odd and even numbers

--- hellogit.c
+++ hellogit.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 
 int main(void){
-	printf("Hello Git!");
-	printf("Hello Hotfix!");
+	int num;
+	printf("Hello Git! \n");
+	printf("Hello Hotfix! \n");
+	printf("숫자를 입력하세요: ");
+	scanf("%d", &num);
+	if(num%2==0)
+		printf("짝수입니다.");
+	else
+		printf("홀수입니다.");
 	return 0;}
